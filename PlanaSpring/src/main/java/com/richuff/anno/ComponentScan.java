82235a49c8db464bbd857fb4b34327b8c45167c3
richu <1691401076@qq.com>
2025-04-25 16:00:37 +0800

    chore:add annotation

--- PlanaSpring/src/main/java/com/richuff/anno/ComponentScan.java
+++ PlanaSpring/src/main/java/com/richuff/anno/ComponentScan.java
@@ -1,4 +1,4 @@
-package richuff.anno;
+package com.richuff.anno;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ComponentScan {
-    String value() default "";
+    String value();
 }

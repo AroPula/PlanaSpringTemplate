3904feebd75b9f998b4d23467dc26a80cdc2bd1b
richu <1691401076@qq.com>
2025-05-02 22:57:31 +0800

    chore:add mapper test

---
+++ PlanaMybatis/src/main/java/com/richuff/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.richuff.mapper;
+
+import com.richuff.entity.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<User> list();
+
+    User findUserById(Integer id);
+
+    void addUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(Integer id);
+}
